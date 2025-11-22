c98595653eb58f8f70afe393ac6340480fa742cb
princekumar39 <singhaniaprince39@gmail.com>
2025-11-22 09:05:24 +0530

    Initial commit for new repo

---
+++ src/Scripts/reviews.js
@@ -0,0 +1,52 @@
+export const customerReviews = [
+    {
+      "name": "Prince Singhania",
+      "location": "Delhi, IND",
+      "message": "TelMed Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "Aryan Rashtogi",
+      "location": "Chennai, IND",
+      "message": "I found the perfect specialist for my condition through Health Plus. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Alica mishra",
+      "location": "Mumbai, IND",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Abhinav Sharma",
+      "location": "Noida, IND",
+      "message": "TelMed Plus is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+    {
+      "name": "Jane Brown",
+      "location": "Grugram, IND",
+      "message": "I used to dread dental visits, but TelMed Plus made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+    },
+    {
+      "name": "Rihani Sharma",
+      "location": "Noida, IND",
+      "message": "Finally, a healthcare platform that puts patients first! The heart disease service provided comprehensive care and regular follow-ups. Thank you, TelMed Plus!"
+    },
+    {
+      "name": "Mary Mishra",
+      "location": "Chandigarh, IND",
+      "message": "I've been using TelMed Plus for a variety of health concerns, and each time, I received exceptional care. It's my go-to for all medical needs!"
+    },
+    {
+      "name": "Deviti Das",
+      "location": "UP, IND",
+      "message": "I highly recommend TelMed Plus for online consultations. It's convenient, secure, and the doctors are top-notch. Great job, team!"
+    },
+    {
+      "name": "dev patil",
+      "location": "Rajesthan, IND",
+      "message": "The convenience of accessing medical notes online was fantastic. TelMed Plus made managing my health records hassle-free! Best Healthcare services."
+    },
+    {
+      "name": "Raghav Sharma",
+      "location": "Bihar, IND",
+      "message": "As a busy professional, TelMed Plus has been a lifesaver. Quick prescriptions and refills without compromising on quality care. Thank you!"
+    }
+];
